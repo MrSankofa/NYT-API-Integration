e520b7efb00d345d4c6283e384faeb5875926226
brett.cunningham <brett.cunningham@sbevision.com>
2025-01-28 19:58:13 -0500

    completed(all): initial project setup using environment variables, builds, next need controller to test api data retrieval

--- src/main/java/com/altruisticSoftwareDevelopment/NYT/API/Integration/NytApiIntegrationApplication.java
+++ src/main/java/com/altruisticSoftwareDevelopment/NYT/API/Integration/NytApiIntegrationApplication.java
@@ -2,6 +2,9 @@ package com.altruisticSoftwareDevelopment.NYT.API.Integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NytApiIntegrationApplication {
@@ -9,5 +12,12 @@ public class NytApiIntegrationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NytApiIntegrationApplication.class, args);
 	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+
 
 }
---
+++ src/main/java/com/altruisticSoftwareDevelopment/NYT/API/Integration/service/ArticleService.java
@@ -0,0 +1,48 @@
+package com.altruisticSoftwareDevelopment.NYT.API.Integration.service;
+
+import com.altruisticSoftwareDevelopment.NYT.API.Integration.model.Article;
+import com.altruisticSoftwareDevelopment.NYT.API.Integration.model.NytResponse;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ArticleService {
+
+  @Value("${api_key}")
+  private String apiKey;
+
+  @Value("${mostPopularUrl}")
+  private String mostPopularUrl;
+
+  @Autowired
+  RestTemplate restTemplate;
+
+  public List<Article> getMostPopularArticles() {
+    NytResponse response = restTemplate.getForObject(mostPopularUrl + "api-key" + apiKey, NytResponse.class);
+
+    List<Article> articles = new ArrayList<>();
+
+    if(response != null && response.getStatus().equals("OK")) {
+      return response.getResults();
+    } else {
+      return articles;
+    }
+  }
+
+//  @PostConstruct
+//  public void init() {
+////    System.out.println("Resolved API Key: " + apiKey);
+//  }
+
+//  @PostConstruct
+//  public void init() {
+//    String envApiKey = System.getenv("NYT_API_KEY");
+//    System.out.println("Environment Variable NYT_API_KEY: " + envApiKey);
+//  }
+}
