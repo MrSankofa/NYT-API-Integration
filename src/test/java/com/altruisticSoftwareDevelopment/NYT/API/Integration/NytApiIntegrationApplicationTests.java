ad332629e5580f790847e24acb7d4e7c1dac1b97
brett.cunningham <brett.cunningham@sbevision.com>
2025-01-28 19:01:49 -0500

    chore(all): second commit

---
+++ src/test/java/com/altruisticSoftwareDevelopment/NYT/API/Integration/NytApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altruisticSoftwareDevelopment.NYT.API.Integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NytApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
